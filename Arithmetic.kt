38dcb4cc4ff77c0d360194f0909566d6b5e82ff3
iamset <49822089+iamset@users.noreply.github.com>
2021-06-04 14:15:01 -0400

    Create Arithmetic.kt

---
+++ Arithmetic.kt
@@ -0,0 +1,9 @@
+open class Arithmetic {
+    // from the description, it implies that the input
+    // will be gotten from the command line.
+    fun add(a:String, b:String):Int = a.toInt() + b.toInt();
+}
+
+class Adder: Arithmetic(){
+
+}
